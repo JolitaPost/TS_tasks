c3db7a83844caeb674bbb705ae824369e9c37798
Jolita Post <jolita.petkeviciute@gmail.com>
2022-10-02 17:38:28 +0300

    ninth task done

---
+++ Task 9/public/js/main.js
@@ -0,0 +1,49 @@
+import LandVehicle from './landVehicle.js';
+import AirVehicle from './airVehicle.js';
+import WaterVehicle from './waterVehicle.js';
+const vehicles = [
+    new LandVehicle({
+        tires: ['Delux 200 m&s', 'Delux 200 m&s', 'Fairtex 200 m&s', 'Fairtex 200 m&s']
+    }, {
+        brand: 'Mersedes',
+        model: 'Vito',
+        year: 2005,
+    }),
+    new LandVehicle({
+        tires: ['Delux 200 m&s', 'Delux 200 m&s', 'Fairtex 200 m&s', 'Fairtex 200 m&s']
+    }, {
+        brand: 'Hyundai',
+        model: 'i30',
+        year: 2007,
+    }),
+    new AirVehicle({
+        maxAltitude: 7000,
+    }, {
+        brand: 'Jeep',
+        model: 'Cherokee',
+        year: 2015,
+    }),
+    new AirVehicle({
+        maxAltitude: 5000,
+    }, {
+        brand: 'Lexus',
+        model: 'rx',
+        year: 2012,
+    }),
+    new WaterVehicle({
+        maxDepth: 50,
+    }, {
+        brand: 'Landrover',
+        model: 'Defender',
+        year: 2011,
+    }),
+    new WaterVehicle({
+        maxDepth: 70,
+    }, {
+        brand: 'Volvo',
+        model: 'xc90',
+        year: 202,
+    }),
+];
+vehicles.forEach(v => console.log(v.toString()));
+//# sourceMappingURL=main.js.map
